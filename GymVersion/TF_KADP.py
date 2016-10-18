cffbb3802620ca3b3e8271ee1e894e729c760865
Steven Hansen <zergylord@gmail.com>
2016-10-17 19:55:27 -0700

    added the proper loss function, but unfinished implementation

--- GymVersion/TF_KADP.py
+++ GymVersion/TF_KADP.py
@@ -7,6 +7,7 @@ plt.ion()
 from scipy.spatial.distance import cdist,pdist
 class KADP(object):
     avg_sim = 0.0
+    #TODO:make kernel and get_value all tf
     def kernel(self,x,X):
         '''Gaussian'''
         dist = np.squeeze(cdist(np.expand_dims(x,0),X)/self.b)
@@ -15,6 +16,17 @@ class KADP(object):
         self.avg_sim = self.avg_sim*.99+sim.sum()*.01
         assert np.all(sim>0), sim[sim<=0]
         return inds,sim
+    ''' calc current value of single state s '''
+    def get_value(self,s):
+        cur_V = np.zeros((self.n_actions,))
+        for a in range(self.n_actions):
+            weights = np.zeros((self.samples_per_action,))
+            inds,vals = self.kernel(s,self.S[a])
+            weights[inds] = vals[inds]
+            cur_W_a = tf.nn.softmax(weights)
+            cur_V[a] = cur_W_a.dot(self.R[a]+self.gamma*self.V[a])
+            aind = cur_V.argmax(0)
+        return cur_V[aind],aind
     def __init__(self,env,W_and_NNI = None):
         self.n_actions = env.action_space.n
         self.samples_per_action = 1000
@@ -56,7 +68,7 @@ class KADP(object):
             self.W,self.NNI = W_and_NNI
         '''create computation graph'''
         tf_W = tf.Variable(self.W)
-        normed_W = tf.nn.softmax(tf_W)
+        normed_W = tf.nn.softmax(tf_W,dim=2)
         V = [tf.zeros((self.n_samples,),dtype=tf.float64)]
         inds = self.NNI
         R_ = tf.gather(self.R_view,inds)
@@ -66,8 +78,14 @@ class KADP(object):
             V.append(tf.reduce_max(tf.reduce_sum(normed_W*(R_+NT_*self.gamma*V_),-1),0))
         self._target = tf.placeholder(tf.float64,shape=(self.n_samples,))
         self.cur_V = V[-1]
-        #TODO: make loss based on TD error
-        loss = tf.reduce_sum(tf.square(self._target-self.cur_V))
+        '''NOTE: This TD error is incorrect for exploratory actions'''
+        self._s = tf.placeholder(tf.float64,shape=(self.s_dim,))
+        self._r = tf.placeholder(tf.float64,shape=(1,))
+        self._sPrime = tf.placeholder(tf.float64,shape=(self.s_dim,))
+        init_value = self.get_value(self._s)
+        final_value = self.get_value(self._sPrime)
+        target = tf.stop_gradient(self._r + self.gamma*final_value)
+        loss = tf.reduce_sum(tf.square(target-init_value))
         self.get_grads = tf.gradients(loss,tf_W)
         self.train_step = tf.train.AdamOptimizer().minimize(loss)
 env = simple_env.Simple()
@@ -76,7 +94,11 @@ sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 cur_time = time.clock()
 for i in range(10):
-    [_,bar,foo] = sess.run([agent.train_step,agent.get_grads,agent.cur_V],feed_dict={agent._target:np.random.randn(agent.n_samples)})
+    s = env.observation_space.sample()
+    _,a = agent.get_value(s)
+    sPrime = env.get_transition(s,a)
+    r = env.get_reward(sPrime)
+    [_,bar,foo] = sess.run([agent.train_step,agent.get_grads,agent.cur_V],feed_dict={agent._s:s,agent._sPrime:sPrime,agent._r:r})
     print(bar,foo,time.clock()-cur_time)
     plt.figure(1)
     plt.clf()

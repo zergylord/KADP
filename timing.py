bb2bccb35b7157798cee767991c7b6a56c3b5396
McClelland-Lab <zergylord@gmail.com>
2016-12-31 16:42:17 -0800

    timing update

--- timing.py
+++ timing.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 import time
 
 sess = tf.Session()
-A = tf.random_uniform((1000,1000))
+A = tf.random_uniform((10000,10000))
 for i in range(int(1e3)):
 	A = (tf.matmul(A,A))
 cur_time = time.clock()
-sess.run(A)
+sess.run(A.op)
 print(time.clock()-cur_time)
